c06342559ccb756242f7e57760e3857b0f68b644
Isak Horvath <48546805+tavro@users.noreply.github.com>
2024-02-06 11:19:58 +0100

    LBPNG: Implement functionality for fetching label

--- lbpng.py
+++ lbpng.py
@@ -23,5 +23,32 @@ def embed_label(path, out, meta):
   with open(out, 'wb') as file:
       file.write(out_content)
 
+def read_label(path):
+  with open(path, 'rb') as file:
+    content = file.read()
+
+  pos = 0
+  while pos < len(content):
+    pos = content.find(b'tEXt', pos)
+    if pos == -1:
+      break
+
+    length = int.from_bytes(content[pos-4:pos], byteorder='big')
+    start = pos + 4
+    end = start + length
+    data = content[start:end]
+
+    if b'CustomLabel\x00' in data:
+      label_start = data.find(b'\x00') + 1
+      label = data[label_start:].decode('utf-8')
+      return label
+
+    pos = end
+
+  return None
+
 label = "Test Label"
 embed_label("test.png", "labeled.png", label)
+
+data = read_label("labeled.png")
+print(data)
